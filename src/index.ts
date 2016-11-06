9cfa8b82c0125eb114bbe3867e99f82389e5c73c
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-11-06 19:45:21 +0100

    yo phovea:migrate-source

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@
  * Created by Samuel Gratzl on 27.08.2015.
  */
 
-import template = require('../caleydo_clue/template');
-import cmds = require('./cmds');
-import databrowser = require('../caleydo_d3/databrowser');
-import selection = require('../caleydo_d3/selectioninfo');
-import cmode = require('../caleydo_clue/mode');
-import $ = require('jquery');
+import * as template from 'phovea_clue/src/template';
+import * as cmds from './cmds';
+import * as databrowser from 'phovea_d3/src/databrowser';
+import * as selection from 'phovea_d3/src/selectioninfo';
+import * as cmode from 'phovea_clue/src/mode';
+import * as $ from 'jquery';
 
 const elems = template.create(document.body, {
   app: 'CLUE',
