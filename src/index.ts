84beffe3c2753d357b817044502936f9f31176f3
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-01-17 18:00:10 +0100

    tslint

--- src/index.ts
+++ src/index.ts
@@ -43,22 +43,22 @@ const elems = template.create(document.body, {
   elems.$main.classed('clue_demo',true);
   const $left = $('aside.left');
 
-  var updateMode = (new_) => {
-    if (new_.exploration < 0.8) {
+  const updateMode = (newMode) => {
+    if (newMode.exploration < 0.8) {
       $left.hide(); //({width: 'hide'});
     } else {
       $left.show(); //({width: 'show'});
     }
   };
 
-  elems.on('modeChanged', (event, new_) => {
-    updateMode(new_);
+  elems.on('modeChanged', (event, newMode) => {
+    updateMode(newMode);
   });
   updateMode(cmode.getMode());
 
   databrowser.makeDropable(<Element>elems.$main.node(), (data, op, pos) => {
     elems.graph.then((graph) => {
-      graph.push(cmds.createAddCmd(elems.$main_ref, data.desc.name, pos));
+      graph.push(cmds.createAddCmd(elems.$mainRef, data.desc.name, pos));
     });
   });
   elems.jumpToStored();
