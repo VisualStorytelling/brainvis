eeb9986dc2b99a7c376e962d08fbb546e9097f18
Maarten van Meersbergen <maartenvm@gmail.com>
2018-10-19 19:30:53 +0200

    got rid of add slide button

--- src/app/provenance-slides/provenance-slides.component.ts
+++ src/app/provenance-slides/provenance-slides.component.ts
@@ -7,7 +7,7 @@ import { ProvenanceService } from '../provenance.service';
 
 @Component({
   selector: 'app-provenance-slides',
-  template: '<button mat-button color="primary" (click)="addSlide()">Add slide</button><div id="slideDeck"></div>',
+  template: '<div id="slideDeck"></div>',
   styleUrls: ['./provenance-slides.component.css'],
   encapsulation: ViewEncapsulation.None
 })
@@ -22,17 +22,8 @@ export class ProvenanceSlidesComponent implements OnInit {
     return this._deck;
   }
 
-  addSlide() {
-    const node = this._deck.graph.current;
-    const slide = new ProvenanceSlide(node.label, 5000, 0, [], node);
-    this._deck.addSlide(slide,
-      this._deck.selectedSlide
-      ? this._deck.slides.indexOf(this._deck.selectedSlide) + 1
-      : this._deck.slides.length);
-  }
-
   ngOnInit() {
     this._deck = new ProvenanceSlidedeck(this.provenance.graph.application, this.provenance.traverser);
-    this._deckViz = new SlideDeckVisualization(this._deck, this.elementRef.nativeElement.children[1]);
+    this._deckViz = new SlideDeckVisualization(this._deck, this.elementRef.nativeElement.children[0]);
   }
 }
