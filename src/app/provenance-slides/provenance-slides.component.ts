4194bf53d6ce0c6e87c759f89f84935cca9fdf5a
Pushpanjali Pawar <pushpanjalip55@gmail.com>
2018-12-10 16:17:34 +0100

    Added Play, Next and Previous Slide button in Slide_deck Player

--- src/app/provenance-slides/provenance-slides.component.ts
+++ src/app/provenance-slides/provenance-slides.component.ts
@@ -1,13 +1,22 @@
-import { Component, ElementRef, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewEncapsulation
+} from '@angular/core';
 
-import { ProvenanceSlide, ProvenanceSlidedeck } from '@visualstorytelling/provenance-core';
+import {
+  ProvenanceSlide,
+  ProvenanceSlidedeck
+} from '@visualstorytelling/provenance-core';
 import { SlideDeckVisualization } from '@visualstorytelling/slide-deck-visualization';
 
 import { ProvenanceService } from '../provenance.service';
 
 @Component({
   selector: 'app-provenance-slides',
-  template: '<button mat-button color="primary" (click)="addSlide()">Add slide</button><div id="slideDeck"></div>',
+  template:
+    '<button mat-button color="primary" (click)="addSlide()"></button><div id="slideDeck"></div>',
   styleUrls: ['./provenance-slides.component.css'],
   encapsulation: ViewEncapsulation.None
 })
@@ -15,8 +24,10 @@ export class ProvenanceSlidesComponent implements OnInit {
   private _deck: ProvenanceSlidedeck;
   private _deckViz: SlideDeckVisualization;
 
-  constructor(private elementRef: ElementRef, private provenance: ProvenanceService) {
-  }
+  constructor(
+    private elementRef: ElementRef,
+    private provenance: ProvenanceService
+  ) {}
 
   get deck() {
     return this._deck;
@@ -25,14 +36,22 @@ export class ProvenanceSlidesComponent implements OnInit {
   addSlide() {
     const node = this._deck.graph.current;
     const slide = new ProvenanceSlide(node.label, 5000, 0, [], node);
-    this._deck.addSlide(slide,
+    this._deck.addSlide(
+      slide,
       this._deck.selectedSlide
-      ? this._deck.slides.indexOf(this._deck.selectedSlide) + 1
-      : this._deck.slides.length);
+        ? this._deck.slides.indexOf(this._deck.selectedSlide) + 1
+        : this._deck.slides.length
+    );
   }
 
   ngOnInit() {
-    this._deck = new ProvenanceSlidedeck(this.provenance.graph.application, this.provenance.traverser);
-    this._deckViz = new SlideDeckVisualization(this._deck, this.elementRef.nativeElement.children[1]);
+    this._deck = new ProvenanceSlidedeck(
+      this.provenance.graph.application,
+      this.provenance.traverser
+    );
+    this._deckViz = new SlideDeckVisualization(
+      this._deck,
+      this.elementRef.nativeElement.children[1]
+    );
   }
 }
