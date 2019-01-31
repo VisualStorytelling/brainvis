10761373f4a0ff3a6a3e226f32f3e411158295d3
Pushpanjali Pawar <pushpanjalip55@gmail.com>
2019-01-31 16:11:20 +0100

    provenance-visualization updated for adding task list

--- src/app/provenance-visualization/provenance-visualization.component.ts
+++ src/app/provenance-visualization/provenance-visualization.component.ts
@@ -1,23 +1,35 @@
-import { Component, ElementRef, OnInit, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewEncapsulation
+} from '@angular/core';
 import { ProvenanceService } from '../provenance.service';
 import { ProvenanceTreeVisualization } from '@visualstorytelling/provenance-tree-visualization';
 
 @Component({
   selector: 'app-provenance-visualization',
-  template: '',
+  templateUrl: './provenance-visualization.component.html',
   styleUrls: ['./provenance-visualization.component.css'],
   encapsulation: ViewEncapsulation.None
 })
 export class ProvenanceVisualizationComponent implements OnInit {
   private _viz: ProvenanceTreeVisualization;
-  constructor(private elementRef: ElementRef, private provenance: ProvenanceService) {
-  }
+  constructor(
+    private elementRef: ElementRef,
+    private provenance: ProvenanceService
+  ) {}
 
+  get viz() {
+    return this._viz;
+  }
+  addNewTask() {
+    this._viz.addTask();
+  }
   ngOnInit() {
     this._viz = new ProvenanceTreeVisualization(
       this.provenance.traverser,
-      this.elementRef.nativeElement,
+      this.elementRef.nativeElement
     );
   }
-
 }
