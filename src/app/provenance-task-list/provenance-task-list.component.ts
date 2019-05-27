fe37ae609fbec124fc69a6e097adba74d8ecf2d6
Pushpanjali Pawar <pushpanjalip55@gmail.com>
2019-05-27 09:29:05 +0200

    Changes for Task List Slider

--- src/app/provenance-task-list/provenance-task-list.component.ts
+++ src/app/provenance-task-list/provenance-task-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, OnInit, ViewEncapsulation } from '@angular/core';
 import { ProvenanceService } from '../provenance.service';
 import { ProvenanceTaskList } from '@visualstorytelling/provenance-task-list';
-
+import { ProvenanceSlide, ProvenanceSlidedeck } from '@visualstorytelling/provenance-core';
 @Component({
   selector: 'app-provenance-task-list',
   template: '',
@@ -9,14 +9,19 @@ import { ProvenanceTaskList } from '@visualstorytelling/provenance-task-list';
   encapsulation: ViewEncapsulation.None
 })
 export class ProvenanceTaskListComponent implements OnInit {
+  private _deck: ProvenanceSlidedeck;
   private _taskList: ProvenanceTaskList;
   constructor(
     private elementRef: ElementRef,
     private provenance: ProvenanceService
-  ) {}
-
+  ) { }
+  get deck() {
+    return this._deck;
+  }
   ngOnInit() {
+    this._deck = new ProvenanceSlidedeck(this.provenance.graph.application, this.provenance.traverser);
     this._taskList = new ProvenanceTaskList(
+      this._deck,
       this.provenance.graph,
       this.elementRef.nativeElement
     );
