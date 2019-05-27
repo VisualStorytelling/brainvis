105b8806a7d45eb72057236a81ce6ff5f7a91981
Tom Klaver <tomklav@gmail.com>
2019-05-27 10:48:24 +0200

    update build

--- styles.js
+++ styles.js
@@ -539,7 +539,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /home/tom/Projects/provenance/brainvis/src/styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! /home/tom/Projects/visualstorytelling/brainvis/src/styles.css */"./src/styles.css");
 
 
 /***/ })
